0119e79168f05dd0d3f84f363c77aecee1557f96
Oh, Sukyeong <osukyeong0109@gmail.com>
2025-02-10 01:46:50 +0900

    design: mypage 초기화면 디자인

--- src/screens/mypage/MyPage.tsx
+++ src/screens/mypage/MyPage.tsx
@@ -1,24 +1,74 @@
-import React from 'react';
-import {View} from 'react-native';
+import { View, Text, Image, TouchableOpacity } from 'react-native';
 import styled from 'styled-components';
+import Profile from './profile/Profile';
+import React from 'react';
 
-const Container = styled(View)`
-  flex: 1;
-  justify-content: center;
-  align-items: center;
-  background-color: ${({theme}) => theme.colors.background};
-`;
-
-const TextStyle = styled.Text`
-  font-size: ${({theme}) => theme.fontSizes.title}px;
-  font-weight: ${({theme}) => theme.fonts.bold};
-  color: ${({theme}) => theme.colors.text};
-`;
+export const ProfileEditItem = ({ title, onPress }: { title: string; onPress: () => void }) => {
+  return (
+    <TouchableOpacity onPress={onPress}>
+      <ProfileEditContainer>
+        <ProfileEditText>{title}</ProfileEditText>
+        <SettingImage source={require('@/assets/image/rightArrow.png')} resizeMode="contain" />
+      </ProfileEditContainer>
+    </TouchableOpacity>
+  );
+};
 
 export default function MyPage() {
   return (
-    <Container>
-      <TextStyle>마이페이지</TextStyle>
-    </Container>
+    <View style={{ flex: 1 }}>
+      <SettingContainer>
+        <SettingImage source={require('@/assets/image/setting.png')} resizeMode="contain" />
+      </SettingContainer>
+      <Profile />
+      <ListContainer>
+        <ProfileEditItem title="프로필 편집" onPress={() => console.log('프로필 편집')} />
+        <ProfileEditItem title="프리미엄" onPress={() => console.log('프리미엄')} />
+        <ProfileEditItem title="커스터마이징" onPress={() => console.log('커스터마이징')} />
+      </ListContainer>
+    </View>
   );
 }
+
+// 설정
+const SettingContainer = styled(View)`
+  width: 90%;
+  display: flex;
+  align-items: flex-end;
+  flex-direction: column;
+  margin: 20px 20px 0 20px;
+  gap: 24px;
+`;
+
+const ListContainer = styled(View)`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 12px;
+  align-self: stretch;
+`;
+
+// 편집
+const ProfileEditContainer = styled(View)`
+  width: 90%;
+  display: flex;
+  padding: 16px;
+  align-items: center;
+  flex-direction: row;
+  margin: 0 20px;
+  border-radius: 10px;
+  background: ${({ theme }) => theme.colors.white};
+`;
+
+const ProfileEditText = styled(Text)`
+  flex: 1;
+  font-size: ${({ theme }) => theme.fontSizes.regular}px;
+  font-weight: 400;
+  color: ${({ theme }) => theme.colors.text};
+`;
+
+// 이미지
+const SettingImage = styled(Image)`
+  width: 24px;
+  height: 24px;
+`;
---
+++ src/screens/mypage/profile/Profile.tsx
@@ -0,0 +1,74 @@
+import React from 'react';
+import {View, Text, Image} from 'react-native';
+import styled from 'styled-components';
+
+export default function Profile() {
+  return (
+    <>
+      <ProfileContainer>
+        <ProfileImageContainer>
+          <ProfileImage
+            source={require('@/assets/image/profileUser.png')}
+            resizeMode="contain"
+          />
+        </ProfileImageContainer>
+        <ProfileTextContainer>
+          <ProfileNickname>명언 좀도둑</ProfileNickname>
+          <ProfileState>상태 메시지를 입력해주세요.</ProfileState>
+        </ProfileTextContainer>
+      </ProfileContainer>
+    </>
+  );
+}
+
+
+const ProfileContainer = styled(View)`
+  width: 90%;
+  display: flex;
+  align-items: flex-start;
+  flex-direction: row;
+  margin: 20px 20px 24px 20px;
+  gap: 24px;
+`;
+
+const ProfileTextContainer = styled(View)`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  gap: 4px;
+  flex: 1 0 0;
+  align-self: stretch;
+`;
+
+const ProfileImageContainer = styled(View)`
+  display: flex;
+  width: 60px;
+  height: 60px;
+  padding: 4px;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+  border-radius: 50px;
+  border: 3px solid ${({theme}) => theme.colors.darkGray};
+  background: #fff;
+`;
+
+const ProfileNickname = styled(Text)`
+  font-size: ${({theme}) => theme.fontSizes.xLarge}px;
+  font-weight: 600;
+  color: ${({theme}) => theme.colors.text};
+`;
+
+const ProfileState = styled(Text)`
+  font-size: ${({theme}) => theme.fontSizes.regular}px;
+  font-weight: 400;
+  color: ${({theme}) => theme.colors.text};
+`;
+
+// 이미지
+const ProfileImage = styled(Image)`
+  width: 44px;
+  height: 44px;
+`;
