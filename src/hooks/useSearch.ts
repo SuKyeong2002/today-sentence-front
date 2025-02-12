3e7a4c3cc375d97b7e59749c5940743d843e03f4
Oh, Sukyeong <osukyeong0109@gmail.com>
2025-02-12 20:05:08 +0900

    chore: 불필요한 파일 삭제 및 코드 정리

--- src/hooks/useSearch.ts
+++ src/hooks/useSearch.ts
@@ -5,22 +5,21 @@ export const useSearch = (type: string, search: string, page = 1, size = 10) => 
     return useQuery(
       ["searchResults", type, search, page],
       async () => {
-        console.log("📌 검색 요청:", { type, search, page, size });
+        console.log("검색 요청:", { type, search, page, size });
   
         const response = await apiClient.get("/api/search/books", {
           params: { type, search, page: page - 1, size },
         });
   
-        console.log("✅ 검색 결과:", response.data);
-  
-        // ✅ `data.data`를 반환해야 올바른 결과 표시 가능
+        console.log("검색 결과:", response.data);
+
         return response.data.data; 
       },
       {
         enabled: !!search && !!type,
         retry: false,
         onError: (error) => {
-          console.error("🔴 검색 오류:", error);
+          console.error("검색 오류:", error);
         },
       }
     );
