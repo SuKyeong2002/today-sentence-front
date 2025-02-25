1401d6c9fa5449492d805663734ca41b2de63598
Oh, Sukyeong <osukyeong0109@gmail.com>
2025-02-26 01:51:09 +0900

    Feat: 폰트 변경 시 자동 로그아웃 경고 문구 추가

--- src/screens/font/FontPage.tsx
+++ src/screens/font/FontPage.tsx
@@ -1,35 +1,36 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import {View, Text} from 'react-native';
 import styled from 'styled-components';
 import BackHeader from '@/components/Header/BackHeader';
-import { useTranslation } from 'react-i18next';
-import { RadioButton } from 'react-native-paper';
-import { useFont } from '@/context/FontContext';
-import { useTheme } from '@/context/ThemeContext';
+import {useTranslation} from 'react-i18next';
+import {RadioButton} from 'react-native-paper';
+import {useFont} from '@/context/FontContext';
+import {useTheme} from '@/context/ThemeContext';
 
 export default function FontPage() {
-  const { t } = useTranslation();
-  const { selectedFont, setSelectedFont } = useFont();
-  const { isDarkMode } = useTheme();
+  const {t} = useTranslation();
+  const {selectedFont, setSelectedFont} = useFont();
+  const {isDarkMode} = useTheme();
 
   const fonts = [
-    { label: '기본', value: 'Pretendard-Regular' },
-    { label: '북엔드 바탕', value: 'BookendBataanRegular' },
-    { label: '온글잎 김콩해', value: 'OnggeulipKimkonghae' },
-    { label: '학교안심 그림일기', value: 'HakgyoansimGeurimilgiTTFR' },
-    { label: '온글잎 위씨리스트', value: 'OnggeulipWicelist' },
-    { label: '교보 손글씨 2020 박도연', value: 'KyoboHandwriting2020pdy' },
+    {label: '기본', value: 'Pretendard-Regular'},
+    {label: '북엔드 바탕', value: 'BookendBataanRegular'},
+    {label: '온글잎 김콩해', value: 'OnggeulipKimkonghae'},
+    {label: '학교안심 그림일기', value: 'HakgyoansimGeurimilgiTTFR'},
+    {label: '온글잎 위씨리스트', value: 'OnggeulipWicelist'},
+    {label: '교보 손글씨 2020 박도연', value: 'KyoboHandwriting2020pdy'},
   ];
 
-  console.log("selectedFont: ", selectedFont);
+  console.log('selectedFont: ', selectedFont);
 
   return (
-    <View style={{ flex: 1, backgroundColor: isDarkMode ? '#000000' : '#F8F9FA' }}>
+    <View
+      style={{flex: 1, backgroundColor: isDarkMode ? '#000000' : '#F8F9FA'}}>
       <BackHeader searchKeyword={t('설정')} />
       <Container>
         {fonts.map((font, index) => (
           <FontWrapper key={index} isDarkMode={isDarkMode}>
-            <FontText style={{ fontFamily: font.value }} isDarkMode={isDarkMode}>
+            <FontText style={{fontFamily: font.value}} isDarkMode={isDarkMode}>
               {font.label}
             </FontText>
             <RadioButton
@@ -40,6 +41,9 @@ export default function FontPage() {
             />
           </FontWrapper>
         ))}
+        <AlertText isDarkMode={isDarkMode}>
+          ⚠️ 폰트 변경 시 로그아웃됩니다. 로그인 후 적용됩니다. ⚠️
+        </AlertText>
       </Container>
     </View>
   );
@@ -51,20 +55,28 @@ const Container = styled(View)`
   gap: 12px;
 `;
 
-const FontWrapper = styled(View)<{ isDarkMode: boolean }>`
+const FontWrapper = styled(View)<{isDarkMode: boolean}>`
   flex-direction: row;
   align-items: center;
   padding: 10px;
   justify-content: space-between;
   border-radius: 10px;
   margin: 0 20px;
-  background: ${({ isDarkMode, theme }) =>
+  background: ${({isDarkMode, theme}) =>
     isDarkMode ? theme.colors.text : theme.colors.white};
 `;
 
-const FontText = styled(Text)<{ isDarkMode: boolean }>`
-  font-family: ${({ theme }) => theme.fontFamily};
-  font-size: ${({ theme }) => theme.fontSizes.regular}px;
-  color: ${({ isDarkMode, theme }) =>
+const FontText = styled(Text)<{isDarkMode: boolean}>`
+  font-family: ${({theme}) => theme.fontFamily};
+  font-size: ${({theme}) => theme.fontSizes.regular}px;
+  color: ${({isDarkMode, theme}) =>
     isDarkMode ? theme.colors.white : theme.colors.text};
+`;
+
+const AlertText = styled(Text)<{isDarkMode: boolean}>`
+  font-size: ${({theme}) => theme.fontSizes.regular}px;
+  color: ${({isDarkMode, theme}) =>
+    isDarkMode ? theme.colors.white : theme.colors.text};
+  text-align: center;
+  margin-top: 10px;
 `;
