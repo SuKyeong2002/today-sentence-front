c738efd2474220ec0852c9cc4892fc12cffe522c
Oh, Sukyeong <osukyeong0109@gmail.com>
2025-02-27 16:26:02 +0900

    login button 하단 고정

--- src/screens/auth/LoginPage.tsx
+++ src/screens/auth/LoginPage.tsx
@@ -1,10 +1,22 @@
-import React, { useState, useEffect } from 'react';
-import { Image, View, Text, TextInput, TouchableOpacity, StyleSheet, Alert } from 'react-native';
-import { NavigationProp } from '@react-navigation/native';
+import React, {useState, useEffect} from 'react';
+import {
+  Image,
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+  Alert,
+} from 'react-native';
+import {NavigationProp} from '@react-navigation/native';
 import useAuth from '../../hooks/useAuth';
 
-export default function LoginPage({ navigation }: { navigation: NavigationProp<any> }) {
-  const { handleLogin, message } = useAuth();
+export default function LoginPage({
+  navigation,
+}: {
+  navigation: NavigationProp<any>;
+}) {
+  const {handleLogin, message} = useAuth();
   const [email, setEmail] = useState('');
   const [password, setPasswordState] = useState('');
 
@@ -12,7 +24,7 @@ export default function LoginPage({ navigation }: { navigation: NavigationProp<a
     await handleLogin(email, password);
   };
 
-  console.log("메세지1", message);
+  console.log('메세지1', message);
   useEffect(() => {
     if (message) {
       if (message === '로그인 성공!') {
@@ -26,8 +38,13 @@ export default function LoginPage({ navigation }: { navigation: NavigationProp<a
   return (
     <View style={styles.container}>
       <View style={styles.header}>
-        <Text style={styles.subtitle}>당신의 하루를 특별하게 만들어 줄 한 문장</Text>
-        <Image source={require('../../assets/image/NewLogoFrame.png')} style={styles.logoImage} />
+        <Text style={styles.subtitle}>
+          당신의 하루를 특별하게 만들어 줄 한 문장
+        </Text>
+        <Image
+          source={require('../../assets/image/NewLogoFrame.png')}
+          style={styles.logoImage}
+        />
       </View>
       <TextInput
         style={styles.input}
@@ -48,11 +65,11 @@ export default function LoginPage({ navigation }: { navigation: NavigationProp<a
         onChangeText={setPasswordState}
       />
       <View style={styles.footerLinks}>
-        <TouchableOpacity onPress={() => navigation.navigate("EmailFind")}>
+        <TouchableOpacity onPress={() => navigation.navigate('EmailFind')}>
           <Text style={styles.footerLinkText}>이메일 찾기</Text>
         </TouchableOpacity>
         <Text style={styles.footerDivider}> | </Text>
-        <TouchableOpacity onPress={() => navigation.navigate("PasswordFind")}>
+        <TouchableOpacity onPress={() => navigation.navigate('PasswordFind')}>
           <Text style={styles.footerLinkText}>비밀번호 찾기</Text>
         </TouchableOpacity>
         <Text style={styles.footerDivider}> | </Text>
@@ -60,77 +77,84 @@ export default function LoginPage({ navigation }: { navigation: NavigationProp<a
           <Text style={styles.footerLinkText}>회원가입</Text>
         </TouchableOpacity>
       </View>
-
-      <TouchableOpacity style={styles.loginButton} onPress={onLoginPress}>
-        <Text style={styles.loginButtonText}>로그인</Text>
-      </TouchableOpacity>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity style={styles.loginButton} onPress={onLoginPress}>
+          <Text style={styles.loginButtonText}>로그인</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-    logoImage : {
-        justifyContent: 'center',
-        padding: 20,
-    },
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#F7F7F7',
-        padding: 20,
-      },
-      header: {
-        alignItems: 'center',
-        marginBottom: 40,
-      },
-      subtitle: {
-        fontSize: 12,
-        color: '#BDBDBD',
-        marginBottom: 5,
-      },
-      logo: {
-        fontSize: 24,
-        fontWeight: 'bold',
-        color: '#5A403D',
-      },
-      input: {
-        color: "#000",
-        width: '100%',
-        height: 50,
-        borderWidth: 1,
-        borderColor: '#E0E0E0',
-        borderRadius: 8,
-        paddingHorizontal: 15,
-        marginBottom: 15,
-        backgroundColor: '#FFF',
-      },
-      footerLinks: {
-        flexDirection: 'row',
-        justifyContent: 'center',
-        marginTop: 10,
-        marginBottom: 30,
-      },
-      footerLinkText: {
-        fontSize: 14,
-        color: '#828282',
-      },
-      footerDivider: {
-        fontSize: 14,
-        color: '#BDBDBD',
-        marginHorizontal: 5,
-      },
-      loginButton: {
-        width: '100%',
-        height: 50,
-        backgroundColor: '#8A715D',
-        justifyContent: 'center',
-        alignItems: 'center',
-        borderRadius: 8,
-      },
-      loginButtonText: {
-        fontSize: 16,
-        fontWeight: 'bold',
-        color: '#FFFFFF',
-      },
+  logoImage: {
+    justifyContent: 'center',
+    padding: 20,
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F7F7F7',
+    padding: 20,
+  },
+  header: {
+    alignItems: 'center',
+    marginBottom: 40,
+  },
+  subtitle: {
+    fontSize: 12,
+    color: '#BDBDBD',
+    marginBottom: 5,
+  },
+  logo: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#5A403D',
+  },
+  input: {
+    color: '#000',
+    width: '100%',
+    height: 50,
+    borderWidth: 1,
+    borderColor: '#E0E0E0',
+    borderRadius: 8,
+    paddingHorizontal: 15,
+    marginBottom: 15,
+    backgroundColor: '#FFF',
+  },
+  footerLinks: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    marginTop: 10,
+    marginBottom: 30,
+  },
+  footerLinkText: {
+    fontSize: 14,
+    color: '#828282',
+  },
+  footerDivider: {
+    fontSize: 14,
+    color: '#BDBDBD',
+    marginHorizontal: 5,
+  },
+  buttonContainer: {
+    position: 'absolute',
+    bottom: 40,
+    width: '90%',
+    alignSelf: 'center',
+  },
+  loginButton: {
+    width: '100%',
+    height: 50,
+    backgroundColor: '#8A715D',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 8,
+  },
+  loginButtonText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#FFFFFF',
+  },
 });
