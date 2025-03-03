04e437d0293952b02e4a3f9d6a2b9ec68b4c0e83
Oh, Sukyeong <osukyeong0109@gmail.com>
2025-03-03 11:48:05 +0900

    Setting: aab 파일 추출중

--- babel.config.js
+++ babel.config.js
@@ -26,5 +26,10 @@ module.exports = {
       },
     ],
     'react-native-reanimated/plugin',
+    ['@babel/plugin-transform-private-methods', { loose: false }], 
+    ['@babel/plugin-transform-private-property-in-object', { loose: false }],
+    ['@babel/plugin-transform-class-properties', { loose: false }],
+    ['@babel/plugin-proposal-export-namespace-from'], // 추가
+    ['@babel/plugin-proposal-export-default-from'], // 추가
   ],
 };
